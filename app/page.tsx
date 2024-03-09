9937305acd127571e36f26c2454e2cc930beed6c
Parth Kadam <2021.parth.kadam@ves.ac.in>
2024-03-10 00:28:20 +0530

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,23 @@
 import Footer from "@/Components/Footer";
 import { Header } from "@/Components/Header/Header";
 import { Hero } from "@/Components/Hero";
-import Preloader from "@/Components/Preloader";
 import { SignupFormDemo } from "@/Components/contactForm";
 import { Macbook } from "@/Components/macbook-display";
 import StickyCursor from "@/Components/stickyCursor";
-import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
 import { TbChevronUp } from "react-icons/tb";
 import {
   Element,
   animateScroll,
 } from "react-scroll";
-import dynamic from 'next/dynamic'
+//import dynamic from 'next/dynamic'
+import About from "@/Components/About";
+import BackgroundGradientDemo from "@/Components/project-cards";
 
-const AboutComponent = dynamic(() => import('@/Components/About'), { ssr: false });
+/* const AboutComponent = dynamic(() => import('@/Components/About'), { ssr: false }); */
 
-const ProjectComponent = dynamic(() => import('@/Components/project-cards'), { ssr: false });
+/* const ProjectComponent = dynamic(() => import('@/Components/project-cards'), { ssr: false }); */
 
 export default function Home() {
-/*   const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    (async () => {
-      setTimeout(() => {
-        setIsLoading(false);
-        window.scrollTo(0, 0);
-      }, 2500);
-    })();
-  }, []); */
 
   const options = {
     duration: 3000,
@@ -45,10 +34,7 @@ export default function Home() {
         <a onClick={scrollToTop} className="flex items-center justify-center w-12 h-12 rounded-full bg-gray-800 text-white text-4xl transition duration-300 ease-in-out hover:bg-gray-700 hover:text-gray-300"> 
           <TbChevronUp />
         </a>
-      </div>
-{/*        <AnimatePresence mode="wait">
-        {isLoading && <Preloader />}
-      </AnimatePresence>  */} 
+      </div> 
       <div className="hidden md:block">
         <StickyCursor />
       </div>
@@ -57,13 +43,15 @@ export default function Home() {
         <Hero />
       </Element>
       <Element name="About" className="element">
-        <AboutComponent />
+        {/* <AboutComponent /> */}
+        <About />
       </Element>
       <Element name="Skills" className="element">
         <Macbook />
       </Element>
       <Element name="Project" className="element">
-        <ProjectComponent />
+        {/* <ProjectComponent /> */}
+        <BackgroundGradientDemo />
       </Element>
       <Element name="Contact" className="element">
         <SignupFormDemo />
